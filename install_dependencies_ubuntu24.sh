621be44460801d6cda99f0df0c8447e504a15b4a
Yaoyu <yaoyuh@andrew.cmu.edu>
2026-02-19 10:22:21 -0500

    Change versioning.

--- install_dependencies_ubuntu24.sh
+++ install_dependencies_ubuntu24.sh
@@ -20,7 +20,6 @@ sudo apt-get install -y \
     curl dpkg-dev git lsb-release openssh-server rsync sshpass tmux tmuxp
 
 # With the Python venv.
-pip install pyyaml vcstool
-pip install setuptools==81.0.0
+pip install pyyaml vcstool "setuptools<=81.0.0"
 
 echo "Done."
